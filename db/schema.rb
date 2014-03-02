aa829149994a34355e6964c6f7d9fb9eb285b75e
Nate Jones <lemaitre86@gmail.com>
2014-03-02 17:35:21 -0500

    Add basic setup/landing page

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140112204108) do
+ActiveRecord::Schema.define(:version => 20140302221935) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+    t.integer  "_v",         :limit => 8
   end
 
 end
